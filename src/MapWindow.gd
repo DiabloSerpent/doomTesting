677c84a2b3e9589f5b74db42cddb059c1f3c7948
Max Herrera <mh1101110@gmail.com>
2024-03-26 13:42:35 -0500

    Add enemy drawing to map

--- src/MapWindow.gd
+++ src/MapWindow.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 var drawn_player_pos = Vector2(0, 0)
+var drawn_enemy_pos_list: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +14,10 @@ func _on_position_change(p: Vector2):
 # Apparently you don't need to call queue_redraw() every frame
 func _draw():
 	draw_circle(drawn_player_pos, 5, Color.AQUA)
+	
+	for e in drawn_enemy_pos_list:
+		draw_circle(e.pos, 6, Color.RED)
+
+
+func _on_game_window_enemy_update(array):
+	drawn_enemy_pos_list = array
