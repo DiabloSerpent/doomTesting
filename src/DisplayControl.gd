e2580a5d3c39318509b586e7128587e0ccb5ac91
Max Herrera <mh1101110@gmail.com>
2024-03-27 10:47:31 -0500

    Move player info label data from script to scene

--- src/DisplayControl.gd
+++ src/DisplayControl.gd
@@ -7,12 +7,4 @@ func _ready():
 	get_parent().player_position_changed.connect(player_pos._on_position_change)
 	
 	var player_move = $PlayerMove
-	player_move.position = Vector2(0, 20)
 	get_parent().player_move_change.connect(player_move._on_move_change)
-	
-	var fps = $FPS
-	fps.position = Vector2(0, 40)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
